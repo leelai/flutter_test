4113aefb55d4026002e3014d29e613492c6e2dea
ALOHA LEE <aloha.app4u@gmail.com>
2020-05-22 06:24:41 +0800

    Update

---
+++ lib/login_route.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'home_route.dart';
+
+class LoginRoute extends StatefulWidget {
+  @override
+  State createState() => _LoginRouteState();
+}
+
+class _LoginRouteState extends State<LoginRoute> {
+  @override
+  Widget build(BuildContext context) {
+    final icon = Image.asset(
+      'images/logo.png',
+      height: 40,
+    );
+
+    final username = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: "Username",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4.0),
+            )),
+      ),
+    );
+
+    final password = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: "Password",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4.0),
+            )),
+      ),
+    );
+
+    final submitButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 5.0,
+        shadowColor: Colors.lime.shade100,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 48.0,
+          child: Text(
+            "LOG IN",
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          color: Colors.lime,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomeRoute()));
+          },
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          children: [icon, username, password, submitButton],
+        ),
+      ),
+    );
+  }
+}
